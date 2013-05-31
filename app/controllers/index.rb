7f7b77fe22d2e2958687592c4dc7d61abf73b953
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-31 16:07:14 -0500

    redo js

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,31 +2,39 @@ get '/' do
   erb :index
 end
 
-post '/game/square' do
-content_type :json
-{:data => false }.to_json
+
+post '/game/update/:game_id' do
+  content_type :json
+  {:data => false }.to_json
 end
 
 
-
-# get 'game/gamestate' do
-  
-#   your_turn?
-#   current_user.id == Game.where(who has less squares picked)
-
-#   content_type :json
-#   {:current_user, :whose_turn, :gamestate}.to_json
-# end
-
-
-get "game/new" do
+get "/game/new" do
   @game = Game.create
   @user_game = UserGame.create(:game_id => @game.id, :user_id => current_user.id)
   redirect "/game/#{@game.id}"
 end
 
-get "game/join_game/:game_id" do
+get "/game/join_game/:game_id" do
   @game = Game.find(params[:game_id])
-  @user_game = UserGame.create(:game_id => @game.id, :user_id => current_user.id)
-  erb :game
+  UserGame.create(:game_id => @game.id, :user_id => current_user.id, :label => 'X')
+  redirect "/game/#{@game.id}"
+end
+
+get "/game/:game_id" do
+  @game = Game.find(params[:game_id])
+  UserGame.create(:game_id => @game.id, :user_id => current_user.id, :label => 'O')
+  erb :current_game
+end
+
+get '/game/gamestate/:game_id' do
+  game = Game.find(params[:game_id])
+  user_label = UserGame.find_by_game_id_and_user_id(game.id, current_user.id).label
+  if game.whose_turn == user_label
+    your_turn = true
+  else
+    your_turn = false
+  end
+  content_type :json
+  {:your_turn => true, :your_label => user_label}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,32 +1,28 @@
 $(document).ready(function() {
   document.getElementById("parent-list").addEventListener("click",function(e) {
     if(e.target && e.target.nodeName == "LI") {
-      $.get('/game/gamestate', function(response){
-        var whose_turn = response['whose_turn'];
-        var board_state = response['board_state'];
-        var current_user =response['current_user'];
-      });
-      if (whose_turn === current_user) {
-        var li_number = e.target.id;
-        if (user === 0){
-          x = $('li#'+li_number).html('X');
+      $.get('/game/gamestate/'+$('#parent-list').data('game_id'), function(response){
+        var your_turn = response['your_turn'];
+        var your_label =response['your_label'];
+        
+        if (your_turn === true) {
+          var li_number = e.target.id;
+          x = $('li#'+li_number).html(your_label);
           $(x).removeClass('btn-info').addClass('btn-warning');
           $('.container').removeClass('player_2').addClass('player_1');
+
+          var square = e.target.id;
+          var data = "{user:"+your_label+",response:"+square+"}";
+          $.post('/game/update/'+$('#parent-list').data('game_id'), data, function(response) {
+            if (response['data'] === true) {
+              $('.container').append('<h1> Player ' + (your_label +1 )+ ' has won! </h1>');
+            }
+          });
         }
-        else {
-          O = $('li#'+li_number).html('O');
-          $(O).removeClass('btn-info').addClass('btn-danger');
-          $('.container').removeClass('player_1').addClass('player_2');
-        }
-        var square = e.target.id;
-        var data = "{user:"+user+",response:"+square+"}";
-        $.post('/game/square', data, function(response) {
-          if (response['data'] === true) {
-            $('.container').append('<h1> Player ' + (user +1 )+ ' has won! </h1>');
-          }
-        });
-      }
+      });
     }
   });
-
 });
+
+
+
