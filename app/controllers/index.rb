2cd669525bca5e6d6d86aa452269247b1f615e13
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-31 15:21:35 -0500

    setting up base routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,5 +4,29 @@ end
 
 post '/game/square' do
 content_type :json
-{:data => true }.to_json
+{:data => false }.to_json
+end
+
+
+
+get 'game/gamestate' do
+  
+  your_turn?
+  current_user.id == Game.where(who has less squares picked)
+
+  content_type :json
+  {:current_user, :whose_turn, :gamestate}.to_json
+end
+
+
+get "game/new" do
+  @game = Game.create
+  @user_game = UserGame.create(:game_id => @game.id, :user_id => current_user.id)
+  erb :game
+end
+
+get "game/:game_id" do
+  @game = Game.find(params[:game_id])
+  @user_game = UserGame.create(:game_id => @game.id, :user_id => current_user.id)
+  erb :game
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,31 @@
 $(document).ready(function() {
-  var user = 0;
   document.getElementById("parent-list").addEventListener("click",function(e) {
     if(e.target && e.target.nodeName == "LI") {
-          var li_number = e.target.id;
-      if (user === 0){
-          $('li#'+li_number).html('X');
-        }
-      else {
-        $('li#'+li_number).html('O');
-      }
-      var square = e.target.id;
-      var data = "{user:"+user+",response:"+square+"}";
-      $.post('/game/square', data, function(response) {
-        if (response['data'] === true) {
-          $('.container').append('<h1> Player ' + (user +1 )+ ' has won! </h1>');
+      $.get('/game/gamestate', function(response){
+        var whose_turn = response['whose_turn'];
+        var board_state = response['board_state'];
+        var current_user =response['current_user'];
+      });
+      if (whose_turn === current_user) {
+        var li_number = e.target.id;
+        if (user === 0){
+          x = $('li#'+li_number).html('X');
+          $(x).removeClass('btn-info').addClass('btn-warning');
+          $('.container').removeClass('player_2').addClass('player_1');
         }
         else {
-          if (user === 0){
-            user = 1;
-          }
-          else {
-            user = 0;
-          }
+          O = $('li#'+li_number).html('O');
+          $(O).removeClass('btn-info').addClass('btn-danger');
+          $('.container').removeClass('player_1').addClass('player_2');
         }
-      });
+        var square = e.target.id;
+        var data = "{user:"+user+",response:"+square+"}";
+        $.post('/game/square', data, function(response) {
+          if (response['data'] === true) {
+            $('.container').append('<h1> Player ' + (user +1 )+ ' has won! </h1>');
+          }
+        });
+      }
     }
   });
 
