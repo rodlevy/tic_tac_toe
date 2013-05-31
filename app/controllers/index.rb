884f19d4cb6f296c114f29578609fd56f0a464f7
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-31 13:59:19 -0500

    working jquery

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,8 @@
 get '/' do
   erb :index
 end
+
+post '/game/square' do
+content_type :json
+{:data => true }.to_json
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
+  var user = 0;
+  document.getElementById("parent-list").addEventListener("click",function(e) {
+    if(e.target && e.target.nodeName == "LI") {
+          var li_number = e.target.id;
+      if (user === 0){
+          $('li#'+li_number).html('X');
+        }
+      else {
+        $('li#'+li_number).html('O');
+      }
+      var square = e.target.id;
+      var data = "{user:"+user+",response:"+square+"}";
+      $.post('/game/square', data, function(response) {
+        if (response['data'] === true) {
+          $('.container').append('<h1> Player ' + (user +1 )+ ' has won! </h1>');
+        }
+        else {
+          if (user === 0){
+            user = 1;
+          }
+          else {
+            user = 0;
+          }
+        }
+      });
+    }
+  });
 
 });
